70d655829d1802de36acbc4e2f5e7b1548878344
Duongcp5112001 <77434155+Duongcp5112001@users.noreply.github.com>
2023-03-31 01:04:23 +0700

    feat/like-dislike

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,5 +1,6 @@
 const Posts = require('../models/Posts');
 const Group = require('../models/Group');
+const mongoose = require('mongoose')
 
 const PostsController = {
     createPosts: async (req, res) => {
@@ -43,6 +44,108 @@ const PostsController = {
                     ...newPosts._doc,
                 }
             })
+        } catch (err) {
+            console.error(err);
+            return res.status(403);
+        }
+    },
+
+    likeDislikePosts: async (req, res) => {
+        try {
+            const { postsId, action } = req.params;
+            const updateBy = req.decodedId;
+
+            const postsFound = await Posts.findById(postsId);
+
+            const { like, dislike } = postsFound;
+
+            let update = {};
+
+            if (!postsFound) return res.status(403).json({errorCode: "23", msg: 'Posts not found'})
+
+            if (action === 'like') {
+                console.log("like")
+                if (like.map((data) => String(data.user)).includes(updateBy)) {
+                    console.log("like if")
+                    update = {
+                        $pull: {
+                            like: {
+                                user: mongoose.Types.ObjectId(updateBy),
+                            },
+                        },
+                    };
+                } else {
+                    console.log("like else")
+                    update = {
+                        $push: {
+                          like: {
+                            user: mongoose.Types.ObjectId(updateBy),
+                            createdAt: new Date(),
+                          },
+                        },
+              
+                        $pull: {
+                          dislike: {
+                            user: mongoose.Types.ObjectId(updateBy),
+                          },
+                        },
+                    };
+                };
+            } else {
+                if (dislike.map((data) => String(data.user)).includes(updateBy)) {
+                    update = {
+                        $pull: {
+                            like: {
+                                user: mongoose.Types.ObjectId(updateBy),
+                            }
+                        }
+                    };
+                } else {
+                    update = {
+                        $push: {
+                          dislike: {
+                            user: mongoose.Types.ObjectId(updateBy),
+                            createdAt: new Date(),
+                          },
+                        },
+              
+                        $pull: {
+                          like: {
+                            user: mongoose.Types.ObjectId(updateBy),
+                          },
+                        },
+                    };
+                };
+            };
+
+            const updateData = await Posts.findByIdAndUpdate(
+                postsId,
+                update,
+                { new: true, useFindAndModify: false }
+            );
+
+            const result = await Posts.findById(String(updateData._id));
+
+            const likeCount = result.like.length;
+            const dislikeCount = result.dislike.length;
+            const commentsCount = result.comments.length;
+            const viewCount = result.views.length;
+            const comment = result.comments.filter((cmt) => {
+                const createdBy = cmt.createdBy;
+                return createdBy;
+            });
+
+            res.json({
+                msg: "Success!",
+                posts: {
+                    ...result._doc,
+                    likeCount,
+                    dislikeCount,
+                    commentsCount,
+                    viewCount,
+                    comment,
+                }
+            });
         } catch (err) {
             console.error(err);
             return res.status(403);
